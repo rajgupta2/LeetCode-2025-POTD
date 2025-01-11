e7737c8610027c16eddad21b19abb67e9161ee55
Raj Gupta <rajgupta.ckt22@gmail.com>
2025-01-11 22:14:21 +0530

    Added POTD for 11 January 2025

---
+++ January 2025/11 January 2025.cpp
@@ -0,0 +1,24 @@
+/*
+Construct K Palindrome Strings
+Given a string s and an integer k, return true if you can use all the characters in s to construct k palindrome strings or false otherwise.
+*/
+class Solution {
+public:
+    bool canConstruct(string s, int k) {
+        if (s.length() < k) return false;
+        if (s.length() == k) return true;
+
+        vector<int> freq(26);
+        int oddCount = 0;
+
+        for (auto& chr : s) freq[chr - 'a']++;
+
+        for (int i = 0; i < 26; i++) {
+            if (freq[i] % 2 == 1) {
+                oddCount++;
+            }
+        }
+
+        return (oddCount <= k);
+    }
+};
