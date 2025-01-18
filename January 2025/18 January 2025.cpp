58ad5d52383e33de5df282a5dbc7d82b07f2afb4
Raj Gupta <rajgupta.ckt22@gmail.com>
2025-01-18 21:44:49 +0530

    Added POTD for 18 January 2025

---
+++ January 2025/18 January 2025.cpp
@@ -0,0 +1,63 @@
+/*
+Minimum Cost to Make at Least One Valid Path in a Grid
+Given an m x n grid. Each cell of the grid has a sign pointing to the next cell you should visit if you are currently in this cell. The sign of grid[i][j] can be:
+1 which means go to the cell to the right. (i.e go from grid[i][j] to grid[i][j + 1])
+2 which means go to the cell to the left. (i.e go from grid[i][j] to grid[i][j - 1])
+3 which means go to the lower cell. (i.e go from grid[i][j] to grid[i + 1][j])
+4 which means go to the upper cell. (i.e go from grid[i][j] to grid[i - 1][j])
+Notice that there could be some signs on the cells of the grid that point outside the grid.
+You will initially start at the upper left cell (0, 0). A valid path in the grid is a path that starts from the upper left cell (0, 0) and ends at the bottom-right cell (m - 1, n - 1) following the signs on the grid. The valid path does not have to be the shortest.
+You can modify the sign on a cell with cost = 1. You can modify the sign on a cell one time only.
+Return the minimum cost to make the grid have at least one valid path.
+*/
+class Solution {
+public:
+    // Direction vectors: right, left, down, up (matching grid values 1, 2, 3,
+    // 4)
+    vector<vector<int>> dirs = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
+
+    int minCost(vector<vector<int>>& grid) {
+        int numRows = grid.size(), numCols = grid[0].size();
+
+        // Track minimum cost to reach each cell
+        vector<vector<int>> minCost(numRows, vector<int>(numCols, INT_MAX));
+
+        // Use deque for 0-1 BFS - add zero cost moves to front, cost=1 to back
+        deque<pair<int, int>> deque;
+        deque.push_front({0, 0});
+        minCost[0][0] = 0;
+
+        while (!deque.empty()) {
+            auto [row, col] = deque.front();
+            deque.pop_front();
+
+            // Try all four directions
+            for (int dir = 0; dir < 4; dir++) {
+                int newRow = row + dirs[dir][0];
+                int newCol = col + dirs[dir][1];
+                int cost = (grid[row][col] != (dir + 1)) ? 1 : 0;
+
+                // If position is valid and we found a better path
+                if (isValid(newRow, newCol, numRows, numCols) &&
+                    minCost[row][col] + cost < minCost[newRow][newCol]) {
+                    minCost[newRow][newCol] = minCost[row][col] + cost;
+
+                    // Add to back if cost=1, front if cost=0
+                    if (cost == 1) {
+                        deque.push_back({newRow, newCol});
+                    } else {
+                        deque.push_front({newRow, newCol});
+                    }
+                }
+            }
+        }
+
+        return minCost[numRows - 1][numCols - 1];
+    }
+
+private:
+    // Check if coordinates are within grid bounds
+    bool isValid(int row, int col, int numRows, int numCols) {
+        return row >= 0 && row < numRows && col >= 0 && col < numCols;
+    }
+};
