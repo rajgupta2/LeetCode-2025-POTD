22f4cb5a22062482e99ff5179edfef612a145e4d
Raj Gupta <rajgupta.ckt22@gmail.com>
2025-02-26 21:18:59 +0530

    Added POTD for 26-02-2025

---
+++ February 2025/26 February 2025.cpp
@@ -0,0 +1,38 @@
+/*
+Maximum Absolute Sum of Any Subarray
+You are given an integer array nums. The absolute sum of a subarray [numsl, numsl+1, ..., numsr-1, numsr] is abs(numsl + numsl+1 + ... + numsr-1 + numsr).
+Return the maximum absolute sum of any (possibly empty) subarray of nums.
+Note that abs(x) is defined as follows:
+If x is a negative integer, then abs(x) = -x.
+If x is a non-negative integer, then abs(x) = x.
+*/
+class Solution {
+    public:
+        int maxAbsoluteSum(vector<int>& nums) {
+            int minPrefixSum = INT_MAX, maxPrefixSum = INT_MIN;
+            int prefixSum = 0, maxAbsSum = 0;
+
+            for (int i = 0; i < nums.size(); i++) {
+                // Prefix sum from index 0 to i
+                prefixSum += nums[i];
+
+                // Minimum & Maximum prefix sum we have seen so far
+                minPrefixSum = min(minPrefixSum, prefixSum);
+                maxPrefixSum = max(maxPrefixSum, prefixSum);
+
+                if (prefixSum >= 0) {
+                    // If the prefixSum is positive, we will get the difference
+                    // between prefixSum & minPrefixSum
+                    maxAbsSum =
+                        max(maxAbsSum, max(prefixSum, prefixSum - minPrefixSum));
+                } else if (prefixSum <= 0) {
+                    // If the prefixSum is negative, we will get the absolute
+                    // difference between prefixSum & maxPrefixSum
+                    maxAbsSum = max(maxAbsSum, max(abs(prefixSum),
+                                                   abs(prefixSum - maxPrefixSum)));
+                }
+            }
+
+            return maxAbsSum;
+        }
+    };
