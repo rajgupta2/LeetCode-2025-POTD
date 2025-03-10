2cab574dc3a9d6467dff89e9b6898b192a87ff2d
Raj Gupta <rajgupta.ckt22@gmail.com>
2025-03-10 23:24:10 +0530

    Added POTD for 10 March 2025

---
+++ March 2025/10 March 2025.cpp
@@ -0,0 +1,71 @@
+/*
+Count of Substrings Containing Every Vowel and K Consonants II
+You are given a string word and a non-negative integer k.
+Return the total number of substrings of word that contain every vowel ('a', 'e', 'i', 'o', and 'u') at least once and exactly k consonants.
+*/
+class Solution {
+    private:
+        bool isVowel(char c) {
+            return c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u';
+        }
+
+    public:
+        long countOfSubstrings(string word, int k) {
+            long numValidSubstrings = 0;
+            int start = 0;
+            int end = 0;
+            unordered_map<char, int> vowelCount;
+            int consonantCount = 0;
+
+            vector<int> nextConsonant(word.size());
+            int nextConsonantIndex = word.size();
+            for (int i = word.size() - 1; i >= 0; i--) {
+                nextConsonant[i] = nextConsonantIndex;
+                if (!isVowel(word[i])) {
+                    nextConsonantIndex = i;
+                }
+            }
+
+            while (end < word.size()) {
+                char newLetter = word[end];
+
+                if (isVowel(newLetter)) {
+                    vowelCount[newLetter]++;
+                } else {
+                    consonantCount++;
+                }
+
+                while (consonantCount > k) {
+                    char startLetter = word[start];
+                    if (isVowel(startLetter)) {
+                        vowelCount[startLetter]--;
+                        if (vowelCount[startLetter] == 0) {
+                            vowelCount.erase(startLetter);
+                        }
+                    } else {
+                        consonantCount--;
+                    }
+                    start++;
+                }
+
+                while (start < word.size() && vowelCount.size() == 5 &&
+                       consonantCount == k) {
+                    numValidSubstrings += nextConsonant[end] - end;
+                    char startLetter = word[start];
+                    if (isVowel(startLetter)) {
+                        vowelCount[startLetter]--;
+                        if (vowelCount[startLetter] == 0) {
+                            vowelCount.erase(startLetter);
+                        }
+                    } else {
+                        consonantCount--;
+                    }
+
+                    start++;
+                }
+                end++;
+            }
+
+            return numValidSubstrings;
+        }
+    };
