c8921926bf41b7aa0b76f3d5c2cf3d70761d5862
Raj Gupta <rajgupta.ckt22@gmail.com>
2025-03-12 23:21:34 +0530

    Added POTD for 12 March 2025

---
+++ March 2025/12 March 2025.cpp
@@ -0,0 +1,21 @@
+/*
+Maximum Count of Positive Integer and Negative Integer
+Given an array nums sorted in non-decreasing order, return the maximum between the number of positive integers and the number of negative integers.
+In other words, if the number of positive integers in nums is pos and the number of negative integers is neg, then return the maximum of pos and neg.
+Note that 0 is neither positive nor negative.*/
+class Solution
+{
+public:
+    int maximumCount(vector<int> &nums)
+    {
+        int positiveCount = 0, negativeCount = 0;
+
+        for (int num : nums)
+        {
+            positiveCount += num > 0;
+            negativeCount += num < 0;
+        }
+
+        return max(positiveCount, negativeCount);
+    }
+};
